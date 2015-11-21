d69734498349ef9b97420dc876e5da6bac2f55af
natedrake13 <john1o1grady@hotmail.com>
2015-11-21 14:43:59 +0000

    Remove AJAXSLT from client side JS

--- public/res/js/main.js
+++ public/res/js/main.js
@@ -30,6 +30,20 @@ $(function(){
         var formData = ($(this).serializeArray());
         /** post the data to the url **/
         $.post('/enc', formData, function(data) {
+            /** parse the xml response to be transformed **/
+            var xml = xmlParse(data);
+            /** get request for xml of xsl file **/
+            $.get(xmlStylesheet, function(response) {
+                /**
+                *   xsl is returned as an XML Object,
+                 *  we need to serialize it, then convert to string
+                 */
+                var xslt = xmlParse(new XMLSerializer().serializeToString(response));
+                /** transform the xml with the xslt **/
+                var returnHTML = xsltProcess(xml, xslt);
+                updatePreviousRequests();
+                $('#response').html(returnHTML);
+            }); // end $.get
         }); // end $.post
     }); // end onsubmit
     /**
@@ -44,5 +58,18 @@ $(function(){
 function updatePreviousRequests() {
     $.post('/requests', '', function(data) {
         /** parse the xml response to be transformed **/
+        var xml = xmlParse(data);
+        /** get request for xml of xsl file **/
+        $.get(xmlStylesheet, function(response) {
+            /**
+            *   xsl is returned as an XML Object,
+             *  we need to serialize it, then convert to string
+             */
+            var xslt = (new XMLSerializer().serializeToString(response));
+            xslt = xmlParse(xslt);
+            /** transform the xml with the xslt **/
+            var returnHTML = xsltProcess(xml, xslt);
+            $('#requests').html(returnHTML);
+        }); // end $.get
     }); // end $.post
 }
