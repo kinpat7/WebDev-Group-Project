4525fa269dfd51e4272a8be286fe2d0857c0641b
natedrake13 <john1o1grady@hotmail.com>
2015-11-21 14:49:44 +0000

    removed AJAXSLT calls from client side JS

--- public/res/js/main.js
+++ public/res/js/main.js
@@ -2,13 +2,8 @@
  *  main.js
  *  @author John O'Grady
  *  @date 10/11/2015
- *  @note controls ajax calls,
- *      uses google AJAXSLT to transform
- *      ajax xml responses with xslt
+ *  @note controls ajax calls
  */
-
-/** GloBal variables **/
-var xmlStylesheet = 'https://cipher-com.herokuapp.com/style.xsl';
 
 /** $(document).onready **/
 $(function(){
@@ -28,48 +23,20 @@ $(function(){
     $('#encrypt-form').submit(function() {
         /** serialize the form data **/
         var formData = ($(this).serializeArray());
-        /** post the data to the url **/
         $.post('/enc', formData, function(data) {
-            /** parse the xml response to be transformed **/
-            var xml = xmlParse(data);
-            /** get request for xml of xsl file **/
-            $.get(xmlStylesheet, function(response) {
-                /**
-                *   xsl is returned as an XML Object,
-                 *  we need to serialize it, then convert to string
-                 */
-                var xslt = xmlParse(new XMLSerializer().serializeToString(response));
-                /** transform the xml with the xslt **/
-                var returnHTML = xsltProcess(xml, xslt);
-                updatePreviousRequests();
-                $('#response').html(returnHTML);
-            }); // end $.get
-        }); // end $.post
-    }); // end onsubmit
+        });
+    });
     /**
      *  load previous requests
      *
      */
-     $('#requests').ready(function(event) {
-         updatePreviousRequests();
-     });
+    $('#requests').ready(function(event) {
+        updatePreviousRequests();
+    });
 });
 
 function updatePreviousRequests() {
     $.post('/requests', '', function(data) {
-        /** parse the xml response to be transformed **/
-        var xml = xmlParse(data);
-        /** get request for xml of xsl file **/
-        $.get(xmlStylesheet, function(response) {
-            /**
-            *   xsl is returned as an XML Object,
-             *  we need to serialize it, then convert to string
-             */
-            var xslt = (new XMLSerializer().serializeToString(response));
-            xslt = xmlParse(xslt);
-            /** transform the xml with the xslt **/
-            var returnHTML = xsltProcess(xml, xslt);
-            $('#requests').html(returnHTML);
-        }); // end $.get
-    }); // end $.post
+        
+    });
 }
