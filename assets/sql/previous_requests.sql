f25581fc02162e5e73aa535cca7ef70f3aab7057
natedrake13 <john1o1grady@hotmail.com>
2015-11-22 17:00:07 +0000

    remove test inserts

--- assets/sql/previous_requests.sql
+++ assets/sql/previous_requests.sql
@@ -8,7 +8,3 @@ create table if not exists requests (
   ip varchar(32) not null,
   primary key(id)
 );
-
-insert into requests ("original", "encrypted", "requested", "ip") values ('blah', 'blah', '2015/11/15', '46.25.64.89');
-
-insert into requests ("original", "encrypted", "requested", "ip") values ('blah', 'blah', '15/11/2015', '46.25.64.89');
